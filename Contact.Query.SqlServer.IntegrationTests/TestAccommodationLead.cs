45d786e62cbe7bccc5c1d795e40162cffca0b219
Andrew Rea <email@andrewrea.co.uk>
2012-12-24 17:04:00 +0000

    Added the first subscriber to the read layer, which is sql to start building up the read layer.

---
+++ Contact.Query.SqlServer.IntegrationTests/TestAccommodationLead.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using Contact.Query.SqlServer.Subscribers;
+using NUnit.Framework;
+
+namespace Contact.Query.SqlServer.IntegrationTests
+{
+    [TestFixture]
+    public class TestAccommodationLead
+    {
+        private string _connectionString;
+        [SetUp]
+        public void Setup()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["TestDatabase"].ConnectionString;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "truncate table AccommodationLeads";
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        [Test]
+        public void ShouldCreateANewAccommodationLead()
+        {
+            var id = Guid.NewGuid();
+            const string name = "Something";
+            const string email = "test@test.com";
+
+            var handler = new AccommodationLeadCreated(_connectionString);
+            var @event = new Contact.Messages.Events.AccommodationLeadCreated
+                {
+                    AccommodationLeadID = id,
+                    Name = name,
+                    Email = email
+                };
+            handler.Handle(@event);
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "Select * from AccommodationLeads where AccommodationLeadID = @Id";
+                    command.Parameters.AddWithValue("@Id", id);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        Assert.That(reader.GetString(reader.GetOrdinal("Name")), Is.EqualTo(name));
+                        Assert.That(reader.GetString(reader.GetOrdinal("Email")), Is.EqualTo(email));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Contact.Query.SqlServer/Subscribers/AccommodationLeadCreated.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using NServiceBus;
+
+namespace Contact.Query.SqlServer.Subscribers
+{
+    public class AccommodationLeadCreated : IHandleMessages<Contact.Messages.Events.AccommodationLeadCreated>
+    {
+        private readonly string _connectionString;
+
+        public AccommodationLeadCreated(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Handle(Messages.Events.AccommodationLeadCreated message)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText =
+                        "insert into AccommodationLeads (AccommodationLeadId, Name, Email) values (@AccommodationLeadId,@name,@Email)";
+                    command.Parameters.AddWithValue("@AccommodationLeadId", message.AccommodationLeadID);
+                    command.Parameters.AddWithValue("@Name", message.Name);
+                    command.Parameters.AddWithValue("@Email", message.Email);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
