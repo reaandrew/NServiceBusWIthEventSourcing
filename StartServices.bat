5b2c56eb2b16763f5f38d907ba87b6b5a64839a8
Andrew Rea <email@andrewrea.co.uk>
2013-01-04 08:50:21 +0000

    Updated the configs so that they are correct along the one queue to one endpoint

--- StartServices.bat
+++ StartServices.bat
@@ -1,5 +1,14 @@
-start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.ApproveAccLead" NServiceBus.Integration
-start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAccommodationLead" NServiceBus.Integration
-start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAccSupplier" NServiceBus.Integration
-start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAuthenticationWithGeneratedPassword" NServiceBus.Integration
-start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateUser" NServiceBus.Integration
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.ApproveAccLead" NServiceBus.Integration NServiceBus.Distributor
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.ApproveAccLead" NServiceBus.Integration NServiceBus.Worker
+
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAccommodationLead" NServiceBus.Integration NServiceBus.Distributor
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAccommodationLead" NServiceBus.Integration NServiceBus.Worker
+
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAccSupplier" NServiceBus.Integration NServiceBus.Distributor
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAccSupplier" NServiceBus.Integration NServiceBus.Worker
+
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAuthenticationWithGeneratedPassword" NServiceBus.Integration NServiceBus.Distributor
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateAuthenticationWithGeneratedPassword" NServiceBus.Integration NServiceBus.Worker
+
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateUser" NServiceBus.Integration NServiceBus.Distributor
+start C:\src\NServiceBusMessagingExample\Contact\bin\Debug\NServiceBus.Host.exe /endpointName:"Contact.Commands.CreateUser" NServiceBus.Integration NServiceBus.Worker
