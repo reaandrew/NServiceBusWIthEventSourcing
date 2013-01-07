f98bfb1be40e9d6c30990de990f85eb83f838ccd
unknown <area@DDT2FX0S4JVB.ad.laterooms.com>
2013-01-07 19:12:22 +0000

    Stubbed out the body of the class which will persist the audit information to a datastore.

---
+++ Contact.Query.Auditing/AuditProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contact.Messages.Commands;
+using NServiceBus;
+
+namespace Contact.Query.Auditing
+{
+    public class AuditProcessor : IHandleMessages<Contact.Messages.Commands.ApproveAccLead>,
+        IHandleMessages<Contact.Messages.Commands.CreateAccSupplier>,
+        IHandleMessages<Contact.Messages.Commands.CreateAccommodationLead>,
+        IHandleMessages<Contact.Messages.Commands.CreateAuthenticationWithGeneratedPassword>,
+        IHandleMessages<Contact.Messages.Commands.CreateUser>
+    {
+        public void Handle(ApproveAccLead message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(CreateAccSupplier message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(CreateAccommodationLead message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(CreateAuthenticationWithGeneratedPassword message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(CreateUser message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
